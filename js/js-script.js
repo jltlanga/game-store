884e693ff31c4777b1e5726ef86d1678c016caea
jltlanga <julitolanga@gmail.com>
2023-04-01 02:49:39 -0300

    jquery update

--- js/js-script.js
+++ js/js-script.js
@@ -8,3 +8,24 @@ function topo() {
         }
     )
 }
+
+// validar login
+
+function login(){
+    var logado = 0;
+    var usuario = document.getElementById('usuario')[0].value;
+    usuario = usuario.toLowerCase();
+    var senha = document.getElementById('senha').value;
+    senha = senha.toLowerCase();
+
+    // validação de login
+    if(usuario == 'admin' && senha =='12345'){
+        window.location = 'index.html';
+        logado = 1;
+    }
+
+    // verificação da situação logado
+    if(logado == 0){
+        alert('Acesso Negado. Dados incorretos');
+    }
+}
